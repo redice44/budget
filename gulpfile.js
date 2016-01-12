df8cbd2f38bbc47ee87d7d801e7df6e3fe643ba7
Matt Thomson <red.cataclysm@gmail.com>
2016-01-12 15:02:23 -0500

    Express Server up

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,31 @@
 
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const del = require('del');
 
 let serverFiles = [
-  'src/*.js'
+  'src/**/*.js'
 ];
 
-gulp.task('default', () => {
-  return gulp.src(serverFiles)
+let htmlFiles = [
+  'src/**/*.jade'
+];
+
+gulp.task('default', ['clean:app', 'build:server', 'build:html']);
+
+gulp.task('build:server', () => {
+  gulp.src(serverFiles)
     .pipe(babel({
       presets: ['es2015']
     }))
     .pipe(gulp.dest('build'));
+});
+
+gulp.task('build:html', () => {
+  gulp.src(htmlFiles)
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('clean:app', () => {
+  del(['build']);
 });
--- src/app/app.js
+++ src/app/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const appConfig = require('./config/appMain');
+const appRoutes = require('./routes/index');
 
-
+appConfig(app, express);
+appRoutes(app);
 
 app.listen(3000, () => {
   console.log('Express Server listening on port: 3000');
