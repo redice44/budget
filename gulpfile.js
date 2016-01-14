de12b131a4ffdf8bd753d9da3a70cbffbd2e903f
Matt Thomson <red.cataclysm@gmail.com>
2016-01-14 14:02:27 -0500

    SASS Capability

--- gulpfile.js
+++ gulpfile.js
@@ -2,54 +2,69 @@
 
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const sass = require('gulp-sass');
 const del = require('del');
-const browserify = require('browserify');
 
-let serverFiles = [
-  'src/app/app.js',
-  'src/app/config/**/*.js',
-  'src/app/routes/**/*.js'
-];
+let location = {
+  server: [
+    'src/app/app.js',
+    'src/app/config/**/*.js',
+    'src/app/routes/**/*.js'    
+  ],
+  client: [
+    'src/app/public/**/*.js'
+  ],
+  html: [
+    'src/**/*.jade'
+  ],
+  css: [
+    'src/app/public/**/*.scss'
+  ],
+  build: 'build'
+};
 
-let serverOptions = { base: 'src'};
+let options = {
+  server: { base: 'src' },
+  client: { base: 'src' },
+  css: { base: 'src' }
+}
 
-let clientFiles = [
-  'src/app/public/**/*.js'
-];
-
-let clientOptions = { base: 'src'};
-
-let htmlFiles = [
-  'src/**/*.jade'
-];
-
-gulp.task('default', ['clean:app', 'build:server', 'build:client', 'build:html']);
+gulp.task('default', ['clean:app', 'build:server', 'build:client', 'build:html', 'build:css']);
 
 gulp.task('watch', () => {
-  gulp.watch(clientFiles, ['build:client']);
+  gulp.watch(location.server, ['build:server']);
+  gulp.watch(location.client, ['build:client']);
+  gulp.watch(location.html, ['build:html']);
+  gulp.watch(location.css, ['build:css']);
 });
 
 gulp.task('build:server', () => {
-  gulp.src(serverFiles, serverOptions)
+  gulp.src(location.server, options.server)
     .pipe(babel({
       presets: ['es2015']
     }))
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest(location.build));
 });
 
 gulp.task('build:client', () => {
-  gulp.src(clientFiles, clientOptions)
+  gulp.src(location.client, options.client)
     .pipe(babel({
       presets: ['es2015', 'react']
     }))
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest(location.build));
 });
 
 gulp.task('build:html', () => {
-  gulp.src(htmlFiles)
-    .pipe(gulp.dest('build'));
+  gulp.src(location.html)
+    .pipe(gulp.dest(location.build));
+});
+
+gulp.task('build:css', () => {
+  gulp.src(location.css, options.css)
+    .pipe(sass())
+    .pipe(gulp.dest(location.build));
 });
 
 gulp.task('clean:app', () => {
-  del(['build']);
+  del([location.build]);
 });
